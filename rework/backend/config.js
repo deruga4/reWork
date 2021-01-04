4334e09bc8a532e2b892776fc7f32920f9f0fadc
Derek Li <derekli.3628@gmail.com>
2021-01-03 19:58:18 -0500

    backup commit

--- rework/backend/config.js
+++ rework/backend/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'ATLAS_URI': 'mongodb+srv://rework-user:M1cXazdvdSe9v3rf@cluster0-xibsd.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    'ATLAS_URI': 'mongodb+srv://rework-user:srZDjyLKHODjy4yA@cluster0.xibsd.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
+//                  mongodb+srv://rework-user:<password>@cluster0.xibsd.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
--- rework/backend/server.js
+++ rework/backend/server.js
@@ -9,8 +9,17 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = config.ATLAS_URI
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
+// const uri = config.ATLAS_URI
+const uri = 'mongodb+srv://rework-user:srZDjyLKHODjy4yA@cluster0.xibsd.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+})
+
 const connection = mongoose.connection
 connection.once('open', () => {
     console.log("MongoDB database connection established")
